0c781783bee70eef53aad3a0b0248e9b6ceedb21
KingGriloPoke <franciscog363@gmail.com>
2020-04-03 17:38:48 +0100

    Cursor Added (with scroll bug)

--- curriculum/src/app/app.component.ts
+++ curriculum/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'cod-root',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'curriculum';
+  top:any;
+  left:any;
+  expand=false;
+  @HostListener('document:click', ['$event'])
+  onClick($event: any) {
+     this.expand=true;
+     setTimeout(() => {
+      this.expand=false;
+     }, 500)
+ }
+ @HostListener('document:mousemove', ['$event'])
+  onMousemove($event: { pageY: number; pageX: number; }) {
+    this.top=($event.pageY - 10)+ "px";
+    this.left= ($event.pageX - 10)+ "px";
+ }
 }
