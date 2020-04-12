c4cc0cb887d2a073e27aa7cf98eeb85ab56a34ef
KingGriloPoke <franciscog363@gmail.com>
2020-04-12 22:18:32 +0100

    Started Chronological Graph

---
+++ curriculum/src/app/core/Interfaces/issues-data.ts
@@ -0,0 +1,111 @@
+declare module IssuesData {
+
+  export interface User {
+      login: string;
+      id: number;
+      node_id: string;
+      avatar_url: string;
+      gravatar_id: string;
+      url: string;
+      html_url: string;
+      followers_url: string;
+      following_url: string;
+      gists_url: string;
+      starred_url: string;
+      subscriptions_url: string;
+      organizations_url: string;
+      repos_url: string;
+      events_url: string;
+      received_events_url: string;
+      type: string;
+      site_admin: boolean;
+  }
+
+  export interface Label {
+      id: number;
+      node_id: string;
+      url: string;
+      name: string;
+      color: string;
+      default: boolean;
+      description: string;
+  }
+
+  export interface Assignee {
+      login: string;
+      id: number;
+      node_id: string;
+      avatar_url: string;
+      gravatar_id: string;
+      url: string;
+      html_url: string;
+      followers_url: string;
+      following_url: string;
+      gists_url: string;
+      starred_url: string;
+      subscriptions_url: string;
+      organizations_url: string;
+      repos_url: string;
+      events_url: string;
+      received_events_url: string;
+      type: string;
+      site_admin: boolean;
+  }
+
+  export interface Assignee2 {
+      login: string;
+      id: number;
+      node_id: string;
+      avatar_url: string;
+      gravatar_id: string;
+      url: string;
+      html_url: string;
+      followers_url: string;
+      following_url: string;
+      gists_url: string;
+      starred_url: string;
+      subscriptions_url: string;
+      organizations_url: string;
+      repos_url: string;
+      events_url: string;
+      received_events_url: string;
+      type: string;
+      site_admin: boolean;
+  }
+
+  export interface PullRequest {
+      url: string;
+      html_url: string;
+      diff_url: string;
+      patch_url: string;
+  }
+
+  export interface RootObject {
+      url: string;
+      repository_url: string;
+      labels_url: string;
+      comments_url: string;
+      events_url: string;
+      html_url: string;
+      id: number;
+      node_id: string;
+      number: number;
+      title: string;
+      user: User;
+      labels: Label[];
+      state: string;
+      locked: boolean;
+      assignee: Assignee;
+      assignees: Assignee2[];
+      milestone?: any;
+      comments: number;
+      created_at: Date;
+      updated_at: Date;
+      closed_at: Date;
+      author_association: string;
+      body: string;
+      pull_request: PullRequest;
+  }
+
+}
+
