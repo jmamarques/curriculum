8e1f3bded445d4cdfcceb2bf58e2e590473b3105
KingGriloPoke <franciscog363@gmail.com>
2020-04-12 16:29:19 +0100

    Created Contributors Bar Graph close #43

--- curriculum/src/app/core/services/technologies.service.ts
+++ curriculum/src/app/core/services/technologies.service.ts
@@ -2,17 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tecnhologies } from '../Interfaces/tecnhologies';
+import { ContributorsGit } from '../Interfaces/contributors-git';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TechnologiesService {
   private tecnhologiesURL='https://api.github.com/repos/joaoMAMarques/curriculum/languages';
+  private contributors = 'https://api.github.com/repos/joaoMAMarques/curriculum/contributors';
 
   constructor(private http: HttpClient) { }
 
   getTechonlogies(): Observable<Tecnhologies>{
     return this.http.get<Tecnhologies>(this.tecnhologiesURL);
-
   }
+  getContributors(): Observable<ContributorsGit []>{
+    return this.http.get<ContributorsGit []>(this.contributors);
+  }
+
 }
