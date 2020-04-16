ec8edecdc92a2fd3fdac5a738f7a78bed0cf85ac
KingGriloPoke <franciscog363@gmail.com>
2020-04-16 22:32:10 +0100

    Added Progress bar with Animation

--- curriculum/src/app/core/profile-creator/profile-creator.component.ts
+++ curriculum/src/app/core/profile-creator/profile-creator.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-creator.component.scss']
 })
 export class ProfileCreatorComponent implements OnInit {
-
+  progressBarValue :number = 0;
+  stepValue:number= 25;
+  currentPage:number = 0;
   constructor() { }
 
   ngOnInit(): void {
+    this.progressBarValue = 24;
+  }
+  increasePercentage():void{
+    this.progressBarValue = this.progressBarValue +this.stepValue;
+    this.currentPage++;
+  }
+
+  decreasePercentage():void{
+    this.progressBarValue -=this.stepValue;
+    this.currentPage--;
   }
 
 }
