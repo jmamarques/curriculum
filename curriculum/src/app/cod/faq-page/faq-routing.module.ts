62c1c5cd7e82f4718b5ecfa18e5545f166c59a09
João Marques <joaoMAMarques@users.noreply.github.com>
2020-04-28 00:41:17 +0100

    Bug Fixed - change color to accordion
    Signed-off-by: João Marques <joaoMAMarques@users.noreply.github.com>

--- curriculum/src/app/cod/faq-page/faq-routing.module.ts
+++ curriculum/src/app/cod/faq-page/faq-routing.module.ts
@@ -1,12 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {FaqPageComponent} from './faq-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FaqPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class FaqRoutingModule {
+  static components = [
+    FaqPageComponent
+  ];
 }
