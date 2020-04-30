acba3bf0f0f6d325bb0bec9f878292b3496175c0
João Marques <joaoMAMarques@users.noreply.github.com>
2020-04-30 23:29:42 +0100

    create expandCollapse animation from Angular Routing course by PluralSight
    Signed-off-by: João Marques <joaoMAMarques@users.noreply.github.com>

--- curriculum/src/app/app.animation.ts
+++ curriculum/src/app/app.animation.ts
@@ -52,3 +52,31 @@ export const fadeAnimation = [
  * <div *ngIf="error" class="error" [@ngIfAnim]>{{error}}</div>
  */
 export const ngIfAnim = trigger('ngIfAnim', fadeAnimation);
+
+const expandCollapseAnimations = [
+  state(
+    '*',
+    style({
+      // enter
+      height: '*',
+      opacity: 1,
+      'overflow-y': 'hidden',
+    })
+  ),
+  state(
+    'void',
+    style({
+      // leave
+      height: '0',
+      opacity: 0,
+      'overflow-y': 'hidden'
+    })
+  ),
+  transition(':leave', animate('250ms ease-out')),
+  transition(':enter', animate('250ms ease-in'))
+];
+
+/**
+ * Expands content when content exposed by *ngIf
+ */
+export const expandCollapse = trigger('expandCollapse', expandCollapseAnimations);
