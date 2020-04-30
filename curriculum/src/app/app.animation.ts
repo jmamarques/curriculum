22884a99cbf913a46febed35800f84d04d71c29f
João Marques <joaoMAMarques@users.noreply.github.com>
2020-04-30 23:19:28 +0100

    create another fade animation from Angular Routing course by PluralSight
    Signed-off-by: João Marques <joaoMAMarques@users.noreply.github.com>

--- curriculum/src/app/app.animation.ts
+++ curriculum/src/app/app.animation.ts
@@ -29,23 +29,26 @@ export const openCloseAnimation = trigger('openClose', [
   transition('false <=> true', animate(500))
 ]);
 
-export const fadeAnimation = trigger('fadeAnimation', [
-  // Transition between any two states
-  transition('* <=> *', [
-    // Events to apply
-    // Defined style and animation function to apply
-    // Config object with optional set to true to handle when element not yet added to the DOM
-    query(':enter, :leave', style({position: 'fixed', width: '100%', zIndex: 2}), {optional: true}),
-    // group block executes in parallel
-    group([
-      query(':enter', [
-        style({opacity: 1}),
-        animate('0.5s ease-out', style({opacity: 0}))
-      ], {optional: true}),
-      query(':leave', [
-        style({opacity: 0}),
-        animate('0.5s ease-out', style({opacity: 1}))
-      ], {optional: true})
-    ])
+export const fadeAnimation = [
+  transition(':enter', [
+    style({opacity: 0}),
+    animate('300ms ease-in', style({opacity: 1})),
+  ]),
+  transition(':leave', [
+    animate('300ms ease-out', style({opacity: 0}))
   ])
-]);
+];
+
+/**
+ * Our standard animation for exposing content when *ngIf is true
+ * @example
+ * // Component file
+ * import { ngIfAnim } from '@animations';
+ * @Component({
+ *   ...
+ *   animations: [ngIfAnim]
+ * })
+ * // Template
+ * <div *ngIf="error" class="error" [@ngIfAnim]>{{error}}</div>
+ */
+export const ngIfAnim = trigger('ngIfAnim', fadeAnimation);
