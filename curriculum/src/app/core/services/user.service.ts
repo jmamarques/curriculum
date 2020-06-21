ba404c112779a81b8a8863fc47303ddb531936b1
João Marques <joaoMAMarques@users.noreply.github.com>
2020-06-21 11:28:03 +0100

    Bug fixed - restore Grilo picture closes #78
    Signed-off-by: João Marques <joaoMAMarques@users.noreply.github.com>

--- curriculum/src/app/core/services/user.service.ts
+++ curriculum/src/app/core/services/user.service.ts
@@ -17,7 +17,7 @@ export class UserService {
         description: 'O meu percurso tem sido feito de desafios e muita aprendizagem. Já participei em vários projetos académicos e não académicos com o intuito de aumentar o meu conhecimento sobre novas Linguagens e Frameworks. Sou uma pessoa curiosa e sempre disponível para incorporar novas equipas, criar aplicações, resolver problemas e escrever código.',
         current_Function: 'Student',
         icon_profile: 'http://getdrawings.com/free-icon/icone-people-66.png',
-        profile_pic: 'https://media-exp1.licdn.com/dms/image/C4D03AQGX56APJUfdfg/profile-displayphoto-shrink_800_800/0?e=1590624000&v=beta&t=9LmP9Ef_EM94hd0dpTT7vjobUmMh7sDqy_RiZMc6ft8',
+        profile_pic: 'https://media-exp1.licdn.com/dms/image/C4D03AQGX56APJUfdfg/profile-displayphoto-shrink_200_200/0?e=1598486400&v=beta&t=5M8goQAqUBGkrIJGChypkivbdzDTQ8Vx2-gM00sFaSg',
         catchPhrase: 'I think you should check my profile. Give it a try'
       },
       {
